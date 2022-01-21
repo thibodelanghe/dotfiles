4ccd8b55d2706717e6061a09ef30928d557c94c4
Thibo De Langhe <thibo.de.langhe@gmail.com>
2022-01-21 20:56:38 +0100

    Started with dotfiles

---
+++ Brewfile
@@ -0,0 +1,24 @@
+# Taps
+tap 'homebrew/cask'
+tap 'homebrew/cask-fonts'
+tap 'homebrew/cask-versions'
+tap 'homebrew/bundle'
+tap 'nicoverbruggen/homebrew-cask'
+
+# Development
+brew 'php'
+brew 'php@8.0'
+brew 'php@7.4'
+brew 'composer'
+brew 'imagemagick'
+
+# Apps
+cask '1password'
+cask 'discord'
+cask 'docker'
+cask 'phpmon'
+cask 'phpstorm'
+cask 'slack'
+cask 'tableplus'
+cask 'visual-studio-code'
+cask 'whatsapp'
---
+++ aliases.zsh
@@ -0,0 +1,40 @@
+# Shortcuts
+alias reloadshell="source $HOME/.zshrc"
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
+alias c="clear"
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+alias library="cd $HOME/Library"
+alias sites="cd $HOME/sites"
+
+# Laravel
+alias a="php artisan"
+alias fresh="php artisan migrate:fresh --seed"
+alias seed="php artisan db:seed"
+alias migrate="php artisan migrate"
+
+# PHP
+alias cfresh="rm -rf vendor/ composer.lock && composer i"
+alias ci="composer install"
+alias cu="composer update"
+
+# JS
+alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
+alias watch="npm run watch"
+alias prod="npm run prod"
+
+# Docker
+alias docker-composer="docker-compose"
+
+# Git
+alias gc="git checkout"
+alias gst="git status"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias pull="git pull"
+alias push="git push"
+alias stash="git stash"
+alias apply="git stash apply"
+alias wip="commit wip"
